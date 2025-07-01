de4bfe408a84f7e9a0fc7d68bfd20390d30a813f
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-07-01 12:02:50 +0530

    Update Fibonacci_Leetcode.java

--- Fibonacci_Leetcode.java
+++ Fibonacci_Leetcode.java
@@ -1,3 +1,4 @@
+//iteration
 class Solution {
     public int fib(int n) {
         int a=0;
@@ -11,6 +12,19 @@ class Solution {
             a=b;
             b=sum;  
         }return sum; 
+        
+    }
+}
+
+
+
+//by recurssion
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        return fib(n-1)+fib(n-2);
         
     }
 }
