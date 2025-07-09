13049172756147b6ee4aa86948df80ce174d5b0e
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-07-10 00:13:12 +0530

    Update 83 Remove Duplicates From Sorted List.java

--- 83 Remove Duplicates From Sorted List.java
+++ 83 Remove Duplicates From Sorted List.java
@@ -12,6 +12,7 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
        if(head==null || head.next==null) return head;
 
+        
        ListNode curr=head;
        while(curr != null && curr.next != null){
         if(curr.val==curr.next.val){
